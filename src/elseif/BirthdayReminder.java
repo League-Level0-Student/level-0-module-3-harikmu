ffab660248f58362b0090d7cd162a20b963fc11e
kanvar22 <league@wts19.attlocal.net>
2018-01-19 16:59:32 -0800

    Birthday Reminder Walk of Fame

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -16,16 +16,28 @@ public class BirthdayReminder {
 		String myBirthday = "April 16th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		String birthday = JOptionPane.showInputDialog("Which birthday do you want?");
 		// 3. Print out what the user typed
-		
+		System.out.println(birthday);
 		// 4. if user asked for "mom"
+		if (birthday == "mom") {
+			System.out.println("November 20, 1972");
+		}
 			//print mom's birthday
 		// 5. if user asked for "dad"
+		if (birthday == "dad") {
+			System.out.println("May 31, 1968");
+		}
 			// print dad's birthday
 		// 6. if user asked for your name
+		if (birthday == "hari") {
+			System.out.println("June 19, 2005");
+		}
+			
 			// print myBirthday
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+		else {
+			System.out.println("Sorry, I don't remember that person's birthday!");
+		}
 	} 
 }
--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -8,12 +8,18 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
-		Robot rob = new Robot();
+		Robot r2d2 = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
 
-		// 2. Make the robot draw a star shape. Hint: 144.
+		r2d2.setX(90);
 
+		// 2. Make the robot draw a star shape. Hint: 144.
+		r2d2.setPenWidth(100);
+		r2d2.penDown();
+		r2d2.move(144);
+		r2d2.setAngle(150);
+		r2d2.move(144);
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
